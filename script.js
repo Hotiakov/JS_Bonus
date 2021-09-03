564f4de6babffe3fd55524f9af0db499d0027717
Hotiakov <54938333+Hotiakov@users.noreply.github.com>
2021-09-03 16:08:59 +0300

    version1

---
+++ script.js
@@ -0,0 +1,111 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    "use strict";
+    const moviesSelect = document.getElementById('movies__select'),
+        cardsContainer = document.querySelector('.cards__container');
+    const getData = async () => {
+        const response = await fetch('./dbHeroes.json');
+        if (!response.ok) {
+            throw new Error('Ошибка при получении данных с сервера');
+        } else {
+            const data = await response.json();
+            return data;
+        }
+    };
+
+    const createMoviesOptions = async () => { //функция создания select со всеми фильмами. Создана на случай, если в json добавился бы новый фильм
+        let options = new Set();
+        const data = await getData();
+        data.forEach(element => {
+            if (element["movies"])
+                element["movies"].forEach((item) => {
+                    options.add(item.trim());
+                });
+        });
+        options = [...options].sort(); //сортируем фильмы в алфавитном порядке(для красоты и удобства)
+        options.forEach((item) => {
+            moviesSelect.insertAdjacentHTML('beforeend', `<option value='${item}'>${item}</option>`);
+        });
+    };
+    createMoviesOptions();
+
+    const filterMeta = (meta) => {//функция получения фильтра по мета-данным
+        const keys = Object.keys(meta);
+        return (obj) => {
+            const newObj = {};
+            keys.forEach(key => {
+                const def = meta[key];
+                const val = def[1](obj[def[0]]);
+                newObj[key] = val;
+            });
+            return newObj;
+        }
+    };
+    const filterData = (data) => { //функция фильтра данных, полученных с сервара
+        const metaData = { //список данных, по которым производится фильтр
+            name: ['name', name => name ? name : 'unknown'], //если каких-то данных нет, меняем их на "неизвестного"
+            photo: ['photo', photo => photo ? photo : 'dbimage/unknown.jpg'],
+            status: ['status', status => status ? status : 'unknown'],
+            realName: ['realName', realName => realName ? realName : 'unknown'],
+            movies: ['movies', movies => movies ? movies : ['None']],
+        };
+        const filter = filterMeta(metaData); //получаем функцию-фильр
+        let filteredData;
+        if (moviesSelect.value === "All") { //если выбрано All - показыавем всех
+            filteredData = data.map(filter);
+        }
+        else {
+            filteredData = data.reduce((newObj, item) => { //создаем новый объект из геров, получая только нужные поля
+                if (moviesSelect.value === "All") {
+                    newObj.push(filter(item));
+                }
+                if (!!item["movies"] && item["movies"].includes(moviesSelect.value)) //проверяем, что персонаж имеет фильм(например, DareDavil его не имеет)
+                    //и что фильм подходит под выбор в select
+                    newObj.push(filter(item));
+                return newObj;
+            }, []);
+        }
+        return filteredData;
+    };
+
+    const createCards = (data) => { //функция создания карточки героя
+        data.forEach(item => {
+            cardsContainer.insertAdjacentHTML('beforeend', `
+                <div class="cards__item" style="background: url('./${item['photo']}') top/cover no-repeat">
+                    <div class="cards__item-wrapper">
+                        <p class="cards__item_name"><span>Name:</span> ${item['name']}</p>
+                        <p class="cards__item_realname"><span>RealName:</span> ${item['realName']}</p>
+                        <p class="cards__item_status"><span>Status:</span> ${item['status']}</p>
+                        <p><span>Movies:</span></p>
+                        <ul class="cards__item_movies">
+                        </ul>
+                    </div>
+                </div>`);
+            const movie = cardsContainer.lastChild.querySelector('.cards__item_movies');
+            item['movies'].forEach(element => {
+                movie.insertAdjacentHTML('beforeend', `
+                        <li class="cards__item_movie">${element}</li>
+                `);
+            });
+        });
+    };
+
+
+    //Вешаем событие изменения выпадающего списка
+    moviesSelect.addEventListener('change', async (e) => {
+        if (moviesSelect.value === 'None') { //Если выбран none - ничего не отображаем
+            cardsContainer.textContent = '';
+        } else {
+            const data = await getData(); //получаем данные с сервера
+            const filteredData = filterData(data); //фильтруем данные
+            cardsContainer.textContent = ''; //стираем все, что было до этого
+            createCards(filteredData); //вставляем новые данные
+        }
+    });
+    //Возможность щелкать по фильмам в карте персанажа и переходить на них
+    cardsContainer.addEventListener('click', e => {
+        if (e.target.tagName === "LI") {
+            moviesSelect.value = e.target.textContent;
+            moviesSelect.dispatchEvent(new Event('change'));
+        }
+    })
+});
