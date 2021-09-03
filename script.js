a854963411031a01dcb65b4a8368afff12d5da9c
Hotiakov <54938333+Hotiakov@users.noreply.github.com>
2021-09-03 22:40:19 +0300

    addFilters

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     const moviesSelect = document.getElementById('movies__select'),
         genderSelect = document.getElementById('gender__select'),
         statusSelect = document.getElementById('status__select'),
-        filterForm = document.getElementById('filter__form'),
+        filterForm = document.querySelector('.filter__form'),
         cardsContainer = document.querySelector('.cards__container');
 
     const getData = async () => {
@@ -17,21 +17,33 @@ document.addEventListener('DOMContentLoaded', async () => {
         }
     };
 
-    const createMoviesOptions = async () => { //функция создания select со всеми фильмами. Создана на случай, если в json добавился бы новый фильм
-        let options = new Set();
+    const createOptions = async () => { //функция создания selectы со всеми фильмами/статусами/гендерами.
+        // Создана на случай, если в json добавился бы новый фильм/статус/гендер(другое написание, неизвестный пол и тп)
+        let optionsMovie = new Set(),
+            optionsGender = new Set(),
+            optionsStatus = new Set();
         const data = await getData();
         data.forEach(element => {
             if (element["movies"])
                 element["movies"].forEach((item) => {
-                    options.add(item.trim());
+                    optionsMovie.add(item.trim());
                 });
+            if (element["status"])
+                optionsStatus.add(element["status"].trim());
+            if (element["gender"])
+                optionsGender.add(element["gender"].trim());
         });
-        options = [...options].sort(); //сортируем фильмы в алфавитном порядке(для красоты и удобства)
-        options.forEach((item) => {
-            moviesSelect.insertAdjacentHTML('beforeend', `<option value='${item}'>${item}</option>`);
-        });
+        optionsMovie = [...optionsMovie].sort(); //сортируем фильмы в алфавитном порядке(для красоты и удобства)
+        const insertOption = (options, select) => {
+            options.forEach((item) => {
+                select.insertAdjacentHTML('beforeend', `<option value='${item}'>${item}</option>`);
+            });
+        }
+        insertOption(optionsMovie, moviesSelect)
+        insertOption(optionsStatus, statusSelect)
+        insertOption(optionsGender, genderSelect)
     };
-    createMoviesOptions();
+    createOptions();
 
     const filterMeta = (meta) => {//функция получения фильтра по мета-данным
         const keys = Object.keys(meta);
@@ -46,29 +58,49 @@ document.addEventListener('DOMContentLoaded', async () => {
         }
     };
     const filterData = (data) => { //функция фильтра данных, полученных с сервара
-        const metaData = { //список данных, по которым производится фильтр
+        const metaDataField = { //список данных, по которым производится фильтр
             name: ['name', name => name ? name : 'unknown'], //если каких-то данных нет, меняем их на "неизвестного"
             photo: ['photo', photo => photo ? photo : 'dbimage/unknown.jpg'],
             status: ['status', status => status ? status : 'unknown'],
             realName: ['realName', realName => realName ? realName : 'unknown'],
             movies: ['movies', movies => movies ? movies : ['None']],
         };
-        const filter = filterMeta(metaData); //получаем функцию-фильр
-        let filteredData;
-        if (moviesSelect.value === "All") { //если выбрано All - показыавем всех
-            filteredData = data.map(filter);
+        const metaDataValues = { //создано для удобства добавления новых фильтров
+            //по сути если добавится другой select, его нужно будет только добавить сюда
+            "movies": moviesSelect.value,
+            "gender": genderSelect.value,
+            "status": statusSelect.value
         }
-        else {
-            filteredData = data.reduce((newObj, item) => { //создаем новый объект из геров, получая только нужные поля
-                if (moviesSelect.value === "All") {
-                    newObj.push(filter(item));
-                }
-                if (!!item["movies"] && item["movies"].includes(moviesSelect.value)) //проверяем, что персонаж имеет фильм(например, DareDavil его не имеет)
-                    //и что фильм подходит под выбор в select
-                    newObj.push(filter(item));
-                return newObj;
-            }, []);
+        const filter = (data, filterValue) => { //функция для фильтра данных по любому из фильтров
+            if (metaDataValues[filterValue] !== "All") { //если выбрано All - не меняем данные
+                data = data.reduce((newObj, item) => { //создаем новый объект из геров, которые попадают под текущий фильтр
+                    if (!!item[filterValue]) { //проверяем, что персонаж имеет данный фильтр(например, DareDavil не имеет фильмов)
+                        if (typeof (item[filterValue]) === "object") { //если массив - используем includes
+                            if (item[filterValue].includes(metaDataValues[filterValue]))//проверяем что поле персонажа подходит под выбор в select
+                                newObj.push(item);
+                        }
+                        else //если не массив - просто проверяем значение
+                            if (!!item[filterValue] && item[filterValue] === metaDataValues[filterValue])
+                                newObj.push(item);
+                    }
+                    return newObj;
+                }, []);
+            }
+            return data;
         }
+        //Фильтруем данные последовательно по каждому из фильтров.
+        //Можно было бы фильтровать одновременно по всем, составляя большое условие, но тогда было бы неудобно внедрять новые фильтры, мне кажется
+        let filteredData = data;
+        Object.keys(metaDataValues).forEach(item => {
+            filteredData = filter(filteredData, item);
+        }); //передаем в функцию текущий фильтр и текущие данные
+
+        const filterFields = filterMeta(metaDataField); //получаем функцию-фильтр, которая "вычленяет" только нужные поля
+        filteredData = filteredData.reduce((newObj, item) => { //создаем новый объект из геров, получая только нужные поля
+            newObj.push(filterFields(item));
+            return newObj;
+        }, []);
+        //так последовательно получаем итоговые данные, в которых есть только нужные поля и которые отфильтрованы по всем фильтрам
         return filteredData;
     };
 
@@ -96,17 +128,23 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 
     //Вешаем событие изменения выпадающего списка
-    moviesSelect.addEventListener('change', async (e) => {
+    filterForm.addEventListener('submit', async e => {
+        e.preventDefault();
         const data = await getData(); //получаем данные с сервера каждый раз(на случай, если данные изменили за время последнего запроса(если бы они были нестатичны))
         const filteredData = filterData(data); //фильтруем данные
         cardsContainer.textContent = ''; //стираем все, что было до этого
-        createCards(filteredData); //вставляем новые данные
+        if (filteredData.length === 0) {
+            alert("По данному фильтр-запросу ничего не найдено:(");
+        }
+        else
+            createCards(filteredData); //вставляем новые данные
     });
     //Возможность щелкать по фильмам в карте персанажа и переходить на них
     cardsContainer.addEventListener('click', e => {
         if (e.target.tagName === "LI") {
             moviesSelect.value = e.target.textContent;
-            moviesSelect.dispatchEvent(new Event('change'));
+            filterForm.dispatchEvent(new Event('submit'));
         }
-    })
+    });
+    filterForm.dispatchEvent(new Event('submit'));//сразу отображаем все фильмы
 });
